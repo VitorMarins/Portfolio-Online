10cdff1da0b3223a402d5b8771693d4a7d26da28
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-08-17 17:09:11 -0300

    feat(aboutme): enhance AboutMe section with improved layout and content

--- src/components/aboutme/index.tsx
+++ src/components/aboutme/index.tsx
@@ -2,27 +2,80 @@ import { myphoto, fundoMensagem } from "../../assets/images";
 
 export default function AboutMe() {
     return (
-        <div className="relative w-full" id="home">
+        <section className="relative w-full overflow-hidden" id="home">
             <img
                 src={myphoto}
-                alt="Foto de Vitor"
-                loading="lazy"
-                className="w-full h-auto z-10 block aspect-video object-cover"
+                alt="Foto de Vitor Marins"
+                fetchPriority="high"
+                loading="eager"
+                decoding="sync"
+                className="block w-full h-auto object-cover"
             />
 
-            <div className="absolute top-12 left-5 w-1/2 z-20" data-aos="fade-right" data-aos-duration="1000">
+            <div
+                className="
+                    absolute
+                    top-1/2
+                    left-4
+                    -translate-y-1/2
+                    w-[55%]
+                    max-w-2xl
+                    sm:left-8
+                    md:left-12
+                    lg:left-16
+                    xl:left-24
+                    z-20
+                "
+                data-aos="fade-right"
+                data-aos-duration="1000"
+            >
                 <div className="relative">
                     <img
                         src={fundoMensagem}
-                        alt="Balao do texto"
-                        loading="lazy"
-                        className="w-full h-auto block aspect-video"
+                        alt=""
+                        aria-hidden="true"
+                        loading="eager"
+                        className="block w-full h-auto"
                     />
-                    <p className="absolute top-0 left-0 w-full h-full flex items-center justify-center p-4 text-1x1 md:text-2xl text-black text-center leading-snug">
-                        Hello, I'm Vitor Barbosa Marins. I have a Associate's Degree in Systems Analysis and Development from Faculdade SENAC PE. I am passionate about programming and have experience in various programming languages and technologies. I am always eager to learn and grow in the field of software development.
-                    </p>
+
+                    <div
+                        className="
+                            absolute inset-0
+                            flex flex-col
+                            items-center justify-center
+                            px-4
+                            sm:px-8
+                            md:px-12
+                            text-center text-black
+                        "
+                    >
+                        <h1
+                            className="
+                                text-lg
+                                font-bold
+                                sm:text-2xl
+                                md:text-4xl
+                                lg:text-5xl
+                            "
+                        >
+                            Vitor Marins
+                        </h1>
+
+                        <p
+                            className="
+                                mt-1
+                                text-xs
+                                font-medium
+                                sm:text-base
+                                md:text-xl
+                                lg:text-2xl
+                            "
+                        >
+                            Software Developer
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
